77b561c0640c918c0e708053935d71025ac80bfa
MitarashiDango <mitarashi_dango@mail.matcha-soft.com>
2024-09-22 23:06:11 +0900

    まだ漏れてた

--- Editor/Inspectors/AnimatorControllerModifierEditor.cs
+++ Editor/Inspectors/AnimatorControllerModifierEditor.cs
@@ -7,27 +7,27 @@ namespace MitarashiDango.AvatarUtils
     [CustomEditor(typeof(AnimatorControllerModifier))]
     public class AnimatorControllerModifierEditor : Editor
     {
-        private SerializedProperty modifierOptions;
+        private SerializedProperty layerModifyOptions;
 
         private ReorderableList reorderableList;
 
         private void OnEnable()
         {
-            modifierOptions = serializedObject.FindProperty("modifierOptions");
+            layerModifyOptions = serializedObject.FindProperty("layerModifyOptions");
 
-            reorderableList = new ReorderableList(serializedObject, modifierOptions)
+            reorderableList = new ReorderableList(serializedObject, layerModifyOptions)
             {
                 drawElementCallback = (rect, index, active, focused) =>
                 {
-                    var additionalFaceEmote = modifierOptions.GetArrayElementAtIndex(index);
+                    var additionalFaceEmote = layerModifyOptions.GetArrayElementAtIndex(index);
                     var position = new Rect(rect)
                     {
                         y = rect.y + EditorGUIUtility.standardVerticalSpacing
                     };
                     EditorGUI.PropertyField(position, additionalFaceEmote);
                 },
-                drawHeaderCallback = (rect) => EditorGUI.LabelField(rect, "Animator Controller Modify Options", EditorStyles.boldLabel),
-                elementHeightCallback = index => EditorGUI.GetPropertyHeight(modifierOptions.GetArrayElementAtIndex(index)) + EditorGUIUtility.standardVerticalSpacing * 2
+                drawHeaderCallback = (rect) => EditorGUI.LabelField(rect, "Animator Controller Layer Modify Options", EditorStyles.boldLabel),
+                elementHeightCallback = index => EditorGUI.GetPropertyHeight(layerModifyOptions.GetArrayElementAtIndex(index)) + EditorGUIUtility.standardVerticalSpacing * 2
             };
         }
 
