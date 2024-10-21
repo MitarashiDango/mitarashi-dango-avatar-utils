00804724049b75e68aa35304947513f8a10abbc8
MitarashiDango <mitarashi_dango@mail.matcha-soft.com>
2024-10-21 23:13:21 +0900

    fix

--- Runtime/BlendShapeSet.cs
+++ Runtime/BlendShapeSet.cs
@@ -1,5 +1,5 @@
-
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace MitarashiDango.AvatarUtils
@@ -7,38 +7,44 @@ namespace MitarashiDango.AvatarUtils
     [CreateAssetMenu(fileName = "New BlendShape Set", menuName = "MitarashiDango AvatarUtils/BlendShape Set", order = 1)]
     public class BlendShapeSet : ScriptableObject
     {
-        public List<BlendShape> blendShapes = new List<BlendShape>();
+        [SerializeField]
+        private List<BlendShape> _blendShapes = new List<BlendShape>();
+
+        public string[] GetKeys()
+        {
+            return _blendShapes.Select(blendShape => blendShape.name).ToArray();
+        }
 
         public BlendShape Get(string name)
         {
-            return blendShapes.Find(BlendShape => BlendShape.name == name);
+            return _blendShapes.Find(BlendShape => BlendShape.name == name);
         }
 
         public void Set(string name, float weight)
         {
-            var index = blendShapes.FindIndex(blendShape => blendShape.name == name);
+            var index = _blendShapes.FindIndex(blendShape => blendShape.name == name);
             if (index >= 0)
             {
-                blendShapes[index].weight = weight;
+                _blendShapes[index].weight = weight;
             }
             else
             {
-                blendShapes.Add(new BlendShape(name, weight));
+                _blendShapes.Add(new BlendShape(name, weight));
             }
         }
 
         public void Delete(string name)
         {
-            var index = blendShapes.FindIndex(blendShape => blendShape.name == name);
+            var index = _blendShapes.FindIndex(blendShape => blendShape.name == name);
             if (index >= 0)
             {
-                blendShapes.RemoveAt(index);
+                _blendShapes.RemoveAt(index);
             }
         }
 
         public bool Exists(string name)
         {
-            return blendShapes.Exists(blendShapes => blendShapes.name == name);
+            return _blendShapes.Exists(blendShapes => blendShapes.name == name);
         }
     }
 }
